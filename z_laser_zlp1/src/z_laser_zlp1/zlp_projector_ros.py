0858bd0664e97b24aeab7a7379addc948acf8a11
Behnoosh <behnoosh.sepehrian@gmail.com>
2023-03-13 18:58:44 +0100

    printing cs after getting from projector by name

--- z_laser_zlp1/src/z_laser_zlp1/zlp_projector_ros.py
+++ z_laser_zlp1/src/z_laser_zlp1/zlp_projector_ros.py
@@ -699,6 +699,8 @@ class ZLPProjectorROS(object):
             self.projector.define_coordinate_system(params, False)
 
             cs = self.projector.get_coordinate_system_params(params.name)
+            print("coordinate system",cs)
+
             self.projector.cs_frame_create(cs)
             self.projector.cs_axes_create(cs)
             rospy.loginfo("Coordinate System [{}] loaded".format(params.name))
